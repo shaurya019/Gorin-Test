68d2142b5a2468e1f2ee3c63be8ee1b4c6122a40
shaurya019 <shauryarana2000@gmail.com>
2025-05-10 13:54:30 +0530

    add the firebase auth and stream builder for navigation

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:provider/provider.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'providers/auth_provider.dart' as auth;
+import 'screens/login_screen.dart';
+import 'screens/home_screen.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(
+    ChangeNotifierProvider(
+      create: (_) =>auth.AuthProvider(),
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -14,6 +27,22 @@ class MyApp extends StatelessWidget {
       title: 'Gorin Test App',
       theme: ThemeData(
         primarySwatch: Colors.blue,
+      ),
+      home: StreamBuilder<User?>(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Scaffold(
+              body: Center(child: CircularProgressIndicator()),
+            );
+          }
+
+          if (snapshot.hasData) {
+            return const HomeScreen();
+          } else {
+            return const LoginScreen();
+          }
+        },
       ),
     );
   }
---
+++ lib/providers/auth_provider.dart
@@ -0,0 +1,44 @@
+import 'dart:io';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/cupertino.dart';
+
+class AuthProvider with ChangeNotifier {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final FirebaseFirestore _db = FirebaseFirestore.instance;
+
+  Stream<User?> get authStateChanges => _auth.authStateChanges();
+  User? get user => _auth.currentUser;
+
+  Future<void> login(String email, String password) async {
+    await _auth.signInWithEmailAndPassword(email: email, password: password);
+  }
+
+  Future<void> logout() async {
+    await _auth.signOut();
+  }
+
+  Future<void> register(String name, String email, String password, File? imageFile) async {
+    try {
+      UserCredential result = await _auth.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+
+      User? user = result.user;
+
+      await _db.collection('users').doc(user!.uid).set({
+        'name': name,
+        'email': email,
+        'profileUrl': '',
+      });
+
+      print(" Registration complete for ${user.uid}");
+    } catch (e) {
+      print("Error during registration: $e");
+      rethrow;
+    }
+  }
+
+}
